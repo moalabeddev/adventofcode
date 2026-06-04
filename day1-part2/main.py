371ea4a855c26ffcc14a26c65a2ba8297200f477
moalabeddev <alabedsaid@proton.me>
2026-06-04 14:49:07 -0400

    Refactor main.py by removing unused code
    Removed unused functions and print statements related to revolution counting and zero positions.

--- day1-part2/main.py
+++ day1-part2/main.py
@@ -1,14 +1,10 @@
-import math
 from raw_data import rotation_text
 from library import (
     parse_rotations_to_signed_values, 
     cumulative_rotation_modulo_100, 
-    count_zero_positions,
-    find_revolution_indices,
-    revolution_count_map,
-    revolution_count_list,
-    count_revolutions
+    count_crossings,
 )
+
 
 rotation_commands = rotation_text.splitlines()
 
@@ -16,17 +12,5 @@ signed_rotations = parse_rotations_to_signed_values(rotation_commands)
 
 wrapped_positions = cumulative_rotation_modulo_100(signed_rotations)
 
-zero_count = count_zero_positions(wrapped_positions)
-print(zero_count)
 
-revolution_indices = find_revolution_indices(signed_rotations)
-
-revolution_count_map=revolution_count_map(revolution_indices, rotation_commands)
-
-revolution_count_list=revolution_count_list(revolution_count_map)
-
-revolution_count=count_revolutions(revolution_count_list)
-
-print(revolution_count)
-
-print(zero_count+revolution_count)
+print(count_crossings(signed_rotations, wrapped_positions))
